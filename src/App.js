6c408d0db0499dfef4738fecb2dd5af4ff4104a8
Alinurrr <alinoer.rachman@gmail.com>
2022-03-07 01:37:23 +0700

    custom title and favico

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Router from './router';
 
 function App() {
+
+  useEffect(() => {
+    document.title = "Pronek";
+  }, [])
+
   return (
     <div>
 
